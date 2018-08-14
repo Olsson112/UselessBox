14b8a31f26d3d16a586fa51299aaa477ffc11012
Victor Olsson <victor.olsson@hiveandfive.se>
2018-08-15 00:10:10 +0200

    Add some functions

--- main.ino
+++ main.ino
@@ -1,13 +1,16 @@
 #include <VarSpeedServo.h>
 
-VarSpeedServo head;
+VarSpeedServo head; 
 VarSpeedServo arm;
 
 int headMaxOpen = 90;
-int armMaxOpen = 15;
+int armMaxOpen = 12;
 
 int headMaxClosed = 170;
 int armMaxClosed = 135;
+
+int armPosition = arm.read();
+int headPosition = head.read();
 
 const byte ledpins[3] = {9, 10, 11};
 
@@ -33,21 +36,64 @@ void setup() {
     delay(2000);
 }
 
-void loop() {
-    
-    if(digitalRead(2)) {
-        turnLightOn();
-        head.write(headMaxOpen, 180, true); 
-        arm.write(armMaxOpen, 180, true); 
-        delay(100);
-        turnLightOff();
-        delay(500);
-        arm.write(armMaxClosed, 160, true); 
-        head.write(headMaxClosed, 160, true); 
-    } else {
-        turnLightOff();
-    }
+bool first = true;
 
+void loop() {
+    if(digitalRead(2)) {
+        if(first) {
+            simpleSwitchOff(90, 90, 90, 90);
+        } else {
+            shakeAndClose();
+        }
+        first = !first;
+    }
+}
+
+void shakeAndClose() {
+    turnLightOn();
+    shakeHead(70, 180, 10);
+    headUp(headMaxOpen, 180);
+    armUp(armMaxOpen, 100);
+    turnLightOff();
+    armDown(armMaxClosed, 100);
+    headDown(headMaxClosed, 100);
+}
+
+void simpleSwitchOff(int headOpenSpeed, int armOpenSpeed, int armCloseSpeed, int headCloseSpeed) {
+    turnLightOn();
+    headUp(headMaxOpen, headOpenSpeed); 
+    armUp(armMaxOpen, armOpenSpeed); 
+    delay(100);
+    turnLightOff();
+    delay(500);
+    armDown(armMaxClosed, armCloseSpeed); 
+    headDown(headMaxClosed, headCloseSpeed); 
+}
+
+
+
+void shakeHead(int degrees, int speed, int timesToShake) {
+    bool shouldGoUp = true;
+    for(int i = 0; i <= timesToShake; i++) {
+        if (shouldGoUp) {
+            headUp(head.read() - degrees, speed);
+        } else {
+            headDown(head.read() + degrees, speed);
+        }
+        shouldGoUp = !shouldGoUp;
+    }
+}
+
+void shakeArm(int degrees, int speed, int timesToShake) {
+    bool shouldGoUp = true;
+    for(int i = 0; i <= timesToShake; i++) {
+        if (shouldGoUp) {
+            armUp(arm.read() - degrees, speed);
+        } else {
+            armDown(arm.read() + degrees, speed);
+        }
+        shouldGoUp = !shouldGoUp;
+    }
 }
 
 void turnLightOff() {
@@ -60,4 +106,28 @@ void turnLightOn() {
     analogWrite(ledpins[0], 255);
     analogWrite(ledpins[1], 255);
     analogWrite(ledpins[2], 255);
+}
+
+void headUp(int degrees, int speed) {
+    if(!(degrees > headMaxClosed || degrees < headMaxOpen)) {
+        head.write(degrees, speed, true);
+    }    
+}
+
+void headDown(int degrees, int speed) {
+    if(!(degrees > headMaxClosed || degrees < headMaxOpen)) {
+        head.write(degrees, speed, true);
+    }
+}
+
+void armUp(int degrees, int speed) {
+    if(!(degrees > armMaxClosed || degrees < armMaxOpen)) {
+        arm.write(degrees, speed, true);
+    }
+}
+
+void armDown(int degrees, int speed) {
+    if(!(degrees > armMaxClosed || degrees < armMaxOpen)) {
+        arm.write(degrees, speed, true);
+    }        
 }
